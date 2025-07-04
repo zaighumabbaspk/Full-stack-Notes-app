b16772a67d17fa76c8ef942b0292dd3ddf0f36fa
Zaighum Abbas <zaighumabbas786000@gmail.com>
2025-07-04 09:18:06 +0500

    Update backend package.json

--- Frontend/my-app/src/lib/axios.js
+++ Frontend/my-app/src/lib/axios.js
@@ -1,3 +1,6 @@
 import axios from "axios";
 
-export const api = axios.create({ baseURL: "http://localhost:3000/api" });
+export const api = axios.create({
+  baseURL:
+    "https://95c6b8ce-2f6c-488c-80e8-34c2fd38a4d4-00-oh4zm24gojpd.sisko.replit.dev/api",
+});
